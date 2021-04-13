4051b5b3b093300c918faabf975e0f58ada8bfe6
wlsry45246@gmail.com <wlsry45246@gmail.com>
2021-04-13 15:53:00 +0900

    CPP_List_2

--- CPP_List/CPP_List/CPP_List.cpp
+++ CPP_List/CPP_List/CPP_List.cpp
@@ -18,6 +18,11 @@ using namespace std;
 // [1] <-> [2] <-> [3] <-> [4] <-> [5]  이중 연결 리스트
 // [1] -> [2] -> [3] -> [4] -> [5] -> [1] -> [2] ... 원형 연결 리스트
 
+// 중간 삽입 삭제시 next와 prev를 잘 조작하면 벡터와 달리 간단하다.
+
+// stl에서 구현을 할때 가상의 노드를 하나 만든다.
+// [1] <-> [2] <-> [3] <-> [4] <-> [5] <-> [ _Myhead = end()] <-> [1]
+
 
 class Node
 {
@@ -33,15 +38,24 @@ int main()
 {
     // list(연결 리스트)
     // - list의 동작 원리
-    // - 중간 삽입/삭제
-    // - 처음/끝 삽입/삭제
-    // - 임의 접근
+    // - 중간 삽입/삭제 (good/good)
+    // - 처음/끝 삽입/삭제 (good/good)
+    // - 임의 접근 (i번때 데이터는 어디 있는지 알수 있나? 직접 포인터를 타고 가봐야 한다.)
+    // 비효율적이라 기능적으로 제공하지 않음
 
     list<int> li;
+    list<int>::iterator itRemember;
     
     for (int i = 0; i < 100; i ++ )
     {
-        li.push_back(i);
+        if (i == 50)
+        {
+            itRemember = li.insert(li.end(), i);
+        }
+        else
+        {
+            li.push_back(i);
+        }
     }
 
     // li.push_front(10);  // vector는 처음 삽입이 없다. 왜냐 비효율적임
@@ -56,6 +70,16 @@ int main()
     list<int>::iterator itBegin = li.begin();
     list<int>::iterator itEnd = li.end();
     
+    int* ptrBegin = &(li.front());
+    int* ptrEnd = &(li.back());
+
+    // list<int>::iterator it2 = itBegin + 10; // 포탈을 타고 10번을 가야된다. 존나 멀다
+    // 지원안함 1칸은 지원
+
+    // list<int>::iterator itTest1 = --itBegin; 맨처음에서 뒤로가면 error
+    // list<int>::iterator itTest2 = --itEnd;
+    // list<int>::iterator itTest3 = ++itEnd;
+
     for (list<int>::iterator it = li.begin(); it != li.end(); ++it)
     {
         cout << *it << endl;
@@ -68,6 +92,19 @@ int main()
     li.pop_back();
 
     li.remove(10);      // 10이라는 데이터를 삭제
+    // 중간 삽입이 간단하여 remove를 지원한다.
+    
+
+    // * 임의 접근이 느리다고 했는데 중간 삽입/삭제는 빠르다 그이유는?
+    // 50번 인덱스에 있는 데이터를 삭제한다면
+    list<int>::iterator it = li.begin();
+    for (int i = 0; i < 50; i++)
+        it++;
+    li.erase(it);
+    // 이렇게 50번째 인덱슬를 찾아서 삭제한다면 느리겠지만
+    // 우리가 삭제해야할 인덱스를 이미 찾아놨다면 삭제는 빠르다.
+    // 이를 분간하도록 하자. 위아래 차이
+    li.erase(itRemember);
 
     return 0;
 }
